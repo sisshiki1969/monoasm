2604c94fb2246428d8be0afc2497eade40f01658
monochrome <sisshiki@mac.com>
2019-09-17 01:05:11 +0900

    Impl. finalize() to resolve relocatios.

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # monoasm
-In-memory x86-64 assembler for JIT compiler.
+
+monoasm is a x86-64 run-time code generator engine written in Rust.
+
+You can generate x86-64 machine codes on memory and exexute them as a Rust function from your program.
+
--- src/main.rs
+++ src/main.rs
@@ -42,11 +42,26 @@ enum Mode {
 
 struct Label(usize);
 
+struct Reloc {
+    loc: Option<usize>,
+    disp: Vec<(u8, usize)>, //(size_in_bytes, pos_in_contents)
+}
+
+impl Reloc {
+    fn new() -> Reloc {
+        Reloc {
+            loc: None,
+            disp: vec![],
+        }
+    }
+}
+
 /// http://www.jonathanturner.org/2015/12/building-a-simple-jit-in-rust.html
 struct JitMemory {
     contents: *mut u8,
     counter: usize,
-    reloc: Vec<(u8, usize)>, //(relocation_size, pos_in_contents)
+    label_count: usize,
+    reloc: Vec<Reloc>,
 }
 
 impl JitMemory {
@@ -66,6 +81,7 @@ impl JitMemory {
         JitMemory {
             contents,
             counter: 0,
+            label_count: 0,
             reloc: vec![],
         }
     }
@@ -77,31 +93,51 @@ impl JitMemory {
         print!("\n");
     }
 
+    fn get_mem_addr(&self) -> u64 {
+        self.contents as u64
+    }
+
     fn label(&mut self) -> Label {
-        let label = Label(self.reloc.len());
+        let label = Label(self.label_count);
+        self.label_count += 1;
+        self.reloc.push(Reloc::new());
         label
     }
 
-    fn push(&mut self, val: u8) {
+    fn finalize(&mut self) {
+        let mut relocs: Vec<(usize, i32)> = vec![];
+        for rel in &mut self.reloc {
+            let pos = rel.loc.unwrap();
+            for (size, dest) in &mut rel.disp {
+                let disp = pos as i32 - *dest as i32 - *size as i32;
+                relocs.push((*dest, disp));
+            }
+        }
+        for (dest, disp) in relocs {
+            self.write32(dest, disp);
+        }
+    }
+
+    fn emitb(&mut self, val: u8) {
         let c = self.counter;
         self[c] = val;
         self.counter = c + 1;
     }
 
-    fn push_rd(&mut self, val: u8, r: Reg) {
+    fn emitb_with_rd(&mut self, val: u8, r: Reg) {
         let c = self.counter;
         self[c] = val | ((r as u8) & 0b0111);
         self.counter = c + 1;
     }
-    /*
-        fn pushw(&mut self, val: u16) {
-            let c = self.counter;
-            self[c] = val as u8;
-            self[c + 1] = (val >> 8) as u8;
-            self.counter = c + 2;
-        }
-    */
-    fn pushl(&mut self, val: u32) {
+
+    fn emitw(&mut self, val: u16) {
+        let c = self.counter;
+        self[c] = val as u8;
+        self[c + 1] = (val >> 8) as u8;
+        self.counter = c + 2;
+    }
+
+    fn emitl(&mut self, val: u32) {
         let c = self.counter;
         self[c] = val as u8;
         self[c + 1] = (val >> 8) as u8;
@@ -110,45 +146,33 @@ impl JitMemory {
         self.counter = c + 4;
     }
 
-    fn pushq(&mut self, val: u64) {
-        self.pushl(val as u32);
-        self.pushl((val >> 32) as u32);
+    fn write32(&mut self, loc: usize, val: i32) {
+        let val = val as u32;
+        self[loc] = val as u8;
+        self[loc + 1] = (val >> 8) as u8;
+        self[loc + 2] = (val >> 16) as u8;
+        self[loc + 3] = (val >> 24) as u8;
     }
 
-    /// Encoding: Opcode + rd
-    fn enc_o(&mut self, op: u8, reg: Reg) {
-        self.rexw(Reg::Rax, reg);
-        self.push_rd(op, reg);
-    }
-
-    /// Encoding: M
-    fn enc_m(&mut self, op: u8, mode: Mode, reg_rm: Reg) {
-        self.enc_mr(op, mode, Reg::Rax, reg_rm);
-    }
-
-    /// Encoding: MR or RM
-    fn enc_mr(&mut self, op: u8, mode: Mode, reg: Reg, reg_rm: Reg) {
-        // TODO: If mode != Reg and r/m == 4(rsp/r12), use SIB.
-        // TODO: If mode == Ind and r/m == 5, becomes [rip + disp32].
-        self.rexw(reg, reg_rm);
-        self.push(op);
-        self.modrm(mode, reg, reg_rm);
+    fn emitq(&mut self, val: u64) {
+        self.emitl(val as u32);
+        self.emitl((val >> 32) as u32);
     }
 
     fn modrm_digit(&mut self, mode: Mode, digit: u8, rm: Reg) {
         let modrm = (mode as u8) << 6 | (digit & 0b111) << 3 | (rm as u8) & 0b111;
-        self.push(modrm);
+        self.emitb(modrm);
     }
 
     fn modrm(&mut self, mode: Mode, reg: Reg, rm: Reg) {
         let modrm = (mode as u8) << 6 | ((reg as u8) & 0b111) << 3 | (rm as u8) & 0b111;
-        self.push(modrm);
+        self.emitb(modrm);
     }
 
     fn rexw(&mut self, rex_r: Reg, rex_b: Reg) {
         let rex_prefix =
             0x48 | ((rex_r as u8) & 0b0000_1000) >> 1 | ((rex_b as u8) & 0b0000_1000) >> 3;
-        self.push(rex_prefix);
+        self.emitb(rex_prefix);
     }
 }
 
@@ -159,16 +183,16 @@ impl JitMemory {
             // MOV r/m64, imm32
             (Or::Reg(r), Or::Imm(i)) if i <= 0xffff_ffff => {
                 self.enc_m(0xc7, Mode::Reg, r);
-                self.pushl(i as u32);
+                self.emitl(i as u32);
             }
             (Or::Ind(r), Or::Imm(i)) if i <= 0xffff_ffff => {
                 self.enc_m(0xc7, Mode::Ind, r);
-                self.pushl(i as u32);
+                self.emitl(i as u32);
             }
             // MOV r64, imm64
             (Or::Reg(r), Or::Imm(i)) => {
                 self.enc_o(0xb8, r);
-                self.pushq(i);
+                self.emitq(i);
             }
             // MOV r/m64,r64
             (Or::Reg(r1), Or::Reg(r2)) => {
@@ -185,10 +209,35 @@ impl JitMemory {
         }
     }
 
+    /// Encoding: Opcode + rd
+    #[inline]
+    fn enc_o(&mut self, op: u8, reg: Reg) {
+        self.rexw(Reg::Rax, reg);
+        self.emitb_with_rd(op, reg);
+    }
+
+    /// Encoding: M
+    #[inline]
+    fn enc_m(&mut self, op: u8, mode: Mode, reg_rm: Reg) {
+        self.enc_mr(op, mode, Reg::Rax, reg_rm);
+    }
+
+    /// Encoding: MR or RM
+    #[inline]
+    fn enc_mr(&mut self, op: u8, mode: Mode, reg: Reg, reg_rm: Reg) {
+        // TODO: If mode != Reg and r/m == 4(rsp/r12), use SIB.
+        // TODO: If mode == Ind and r/m == 5, becomes [rip + disp32].
+        self.rexw(reg, reg_rm);
+        self.emitb(op);
+        self.modrm(mode, reg, reg_rm);
+    }
+}
+
+impl JitMemory {
     fn call(&mut self, op: Or) {
         match op {
             Or::Reg(r) => {
-                self.push(0xff);
+                self.emitb(0xff);
                 self.modrm_digit(Mode::Reg, 2, r);
             }
             _ => unimplemented!(),
@@ -196,7 +245,7 @@ impl JitMemory {
     }
 
     fn ret(&mut self) {
-        self.push(0xc3);
+        self.emitb(0xc3);
     }
 
     fn q_rm64_imm32(&mut self, opcode_imm: u8, digit: u8, op1: Or, op2: Or) {
@@ -207,13 +256,13 @@ impl JitMemory {
                     panic!("'XXX r/m64, imm64' does not exists.");
                 }
                 self.rexw(Reg::Rax, r);
-                self.push(opcode_imm);
+                self.emitb(opcode_imm);
                 match op1 {
                     Or::Reg(r) => self.modrm_digit(Mode::Reg, digit, r),
                     Or::Ind(r) => self.modrm_digit(Mode::Ind, digit, r),
                     _ => unimplemented!(),
                 }
-                self.pushl(i as u32);
+                self.emitl(i as u32);
             }
             _ => unimplemented!(),
         }
@@ -262,27 +311,23 @@ impl JitMemory {
     fn jmp(&mut self, dest: Or) {
         match dest {
             Or::Imm(disp) => {
-                self.push(0xe9);
-                self.pushl((disp as i64 - self.counter as i64 - 4) as u32);
+                self.emitb(0xe9);
+                self.emitl((disp as i64 - self.counter as i64 - 4) as u32);
             }
             _ => unimplemented!(),
         }
     }
 
-    fn jne(&mut self, dest: Or) {
-        match dest {
-            Or::Imm(disp) => {
-                self.push(0x0f);
-                self.push(0x85);
-                self.pushl((disp as i64 - self.counter as i64 - 4) as u32);
-            }
-            _ => unimplemented!(),
-        }
+    fn jne(&mut self, dest: Label) {
+        self.emitb(0x0f);
+        self.emitb(0x85);
+        self.reloc[dest.0].disp.push((4, self.counter));
+        self.emitl(0);
     }
 
     fn syscall(&mut self) {
-        self.push(0x0f);
-        self.push(0x05);
+        self.emitb(0x0f);
+        self.emitb(0x05);
     }
 }
 
@@ -300,12 +345,12 @@ impl IndexMut<usize> for JitMemory {
     }
 }
 
-fn get_func_addr() -> (fn() -> i64) {
+fn jit() -> (fn() -> i64) {
     let hello = "Hello World! Are you angry?\n";
     let mut jit: JitMemory = JitMemory::new();
 
     jit.movq(Or::Reg(Reg::Rdi), Or::Imm(1));
-    jit.movq(Or::Reg(Reg::R8), Or::Imm(jit.contents as u64 + 256));
+    jit.movq(Or::Reg(Reg::R8), Or::Imm(jit.get_mem_addr() + 256));
     jit.movq(Or::Reg(Reg::Rax), Or::Imm(hello.as_ptr() as u64));
     jit.movq(Or::Ind(Reg::R8), Or::Reg(Reg::Rax));
     jit.movq(Or::Reg(Reg::Rsi), Or::Ind(Reg::R8));
@@ -315,7 +360,8 @@ fn get_func_addr() -> (fn() -> i64) {
 
     jit.movq(Or::Reg(Reg::R15), Or::Imm(jit.contents as u64 + 256));
     jit.movq(Or::Ind(Reg::R15), Or::Imm(0x40));
-    let label = jit.counter;
+    let label = jit.label();
+    jit.reloc[label.0].loc = Some(jit.counter);
     let putchar_addr = libc::putchar as *const u8 as u64;
     jit.addq(Or::Ind(Reg::R15), Or::Imm(1));
     jit.movq(Or::Reg(Reg::Rax), Or::Imm(putchar_addr));
@@ -323,15 +369,20 @@ fn get_func_addr() -> (fn() -> i64) {
     jit.call(Or::Reg(Reg::Rax));
 
     jit.cmpq(Or::Ind(Reg::R15), Or::Imm(0x60));
-    jit.jne(Or::Imm(label as u64));
+    jit.jne(label);
+
+    jit.movq(Or::Reg(Reg::Rax), Or::Imm(putchar_addr));
+    jit.movq(Or::Reg(Reg::Rdi), Or::Imm('\n' as u64));
+    jit.call(Or::Reg(Reg::Rax));
 
     jit.ret();
+    jit.finalize();
     //jit.p();
     unsafe { mem::transmute(jit.contents) }
 }
 
 fn main() {
-    let func = get_func_addr();
+    let func = jit();
     let ret = func();
     println!("returned value:{}", ret);
 }
